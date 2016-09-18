405d81e0959c8e2a321d726613fd662c83e1a016
aditaufik <aditaufik20@gmail.com>
2016-09-18 22:46:44 +0700

    Add files via upload

---
+++ xitkj2020.ino
@@ -0,0 +1,12 @@
+void setup() {
+  //  initialize digital pin 9 as an output.
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(3000);             // wait for a second
+  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
+  delay(3000);             // wait for a second
+
+}
